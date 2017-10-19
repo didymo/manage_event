d0b861fa9939397b58c3d9a2d9d93332ef1f5c40
Ashley Maher <ashley.maher@didymodesigns.com.au>
2017-10-19 11:58:47 +1100

    This is the initial commit

---
+++ src/Entity/Event.php
@@ -0,0 +1,422 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\manage_event\Entity\Contact.
+ */
+namespace Drupal\manage_event\Entity;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\manage_event\EventInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the Contact entity.
+ *
+ * @ingroup manage_event
+ *
+ * This is the main definition of the entity type. From it, an entityType is
+ * derived. The most important properties in this example are listed below.
+ *
+ * id: The unique identifier of this entityType. It follows the pattern
+ * 'moduleName_xyz' to avoid naming conflicts.
+ *
+ * label: Human readable name of the entity type.
+ *
+ * handlers: Handler classes are used for different tasks. You can use
+ * standard handlers provided by D8 or build your own, most probably derived
+ * from the standard class. In detail:
+ *
+ * - view_builder: we use the standard controller to view an instance. It is
+ *   called when a route lists an '_entity_view' default for the entityType
+ *   (see routing.yml for details. The view can be manipulated by using the
+ *   standard drupal tools in the settings.
+ *
+ * - list_builder: We derive our own list builder class from the
+ *   entityListBuilder to control the presentation.
+ *   If there is a view available for this entity from the views module, it
+ *   overrides the list builder. @todo: any view? naming convention?
+ *
+ * - form: We derive our own forms to add functionality like additional fields,
+ *   redirects etc. These forms are called when the routing list an
+ *   '_entity_form' default for the entityType. Depending on the suffix
+ *   (.add/.edit/.delete) in the route, the correct form is called.
+ *
+ * - access: Our own accessController where we determine access rights based on
+ *   permissions.
+ *
+ * More properties:
+ *
+ *  - base_table: Define the name of the table used to store the data. Make sure
+ *    it is unique. The schema is automatically determined from the
+ *    BaseFieldDefinitions below. The table is automatically created during
+ *    installation.
+ *
+ *  - fieldable: Can additional fields be added to the entity via the GUI?
+ *    Analog to content types.
+ *
+ *  - entity_keys: How to access the fields. Analog to 'nid' or 'uid'.
+ *
+ *  - links: Provide links to do standard tasks. The 'edit-form' and
+ *    'delete-form' links are added to the list built by the
+ *    entityListController. They will show up as action buttons in an additional
+ *    column.
+ *
+ * There are many more properties to be used in an entity type definition. For
+ * a complete overview, please refer to the '\Drupal\Core\Entity\EntityType'
+ * class definition.
+ *
+ * The following construct is the actual definition of the entity type which
+ * is read and cached. Don't forget to clear cache after changes.
+ *
+ * @ContentEntityType(
+ *   id = "content_entity_manage_event",
+ *   label = @Translation("Event entity"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\manage_event\Entity\Controller\EventListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "form" = {
+ *       "add" = "Drupal\manage_event\Form\EventForm",
+ *       "edit" = "Drupal\manage_event\Form\EventForm",
+ *       "delete" = "Drupal\manage_event\Form\EventDeleteForm",
+ *     },
+ *     "access" = "Drupal\manage_event\EventAccessControlHandler",
+ *   },
+ *   base_table = "event",
+ *   admin_permission = "administer manage_event entity",
+ *   fieldable = TRUE,
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "label" = "e_name",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/content_entity_manage_event/{content_entity_manage_event}",
+ *     "edit-form" = "/content_entity_manage_event/{content_entity_manage_event}/edit",
+ *     "delete-form" = "/event/{content_entity_manage_event}/delete",
+ *     "collection" = "/content_entity_manage_event/list"
+ *   },
+ *   field_ui_base_route = "manage_event.event_settings",
+ * )
+ *
+ * The 'links' above are defined by their path. For core to find the corresponding
+ * route, the route name must follow the correct pattern:
+ *
+ * entity.<entity-name>.<link-name> (replace dashes with underscores)
+ * Example: 'entity.content_entity_manage_event.canonical'
+ *
+ * See routing file above for the corresponding implementation
+ *
+ * The 'Contact' class defines methods and fields for the contact entity.
+ *
+ * Being derived from the ContentEntityBase class, we can override the methods
+ * we want. In our case we want to provide access to the standard fields about
+ * creation and changed time stamps.
+ *
+ * Our interface (see EventInterface) also exposes the EntityOwnerInterface.
+ * This allows us to provide methods for setting and providing ownership
+ * information.
+ *
+ * The most important part is the definitions of the field properties for this
+ * entity type. These are of the same type as fields added through the GUI, but
+ * they can by changed in code. In the definition we can define if the user with
+ * the rights privileges can influence the presentation (view, edit) of each
+ * field.
+ */
+class Event extends ContentEntityBase implements EventInterface {
+    /**
+     * {@inheritdoc}
+     *
+     * When a new entity instance is added, set the user_id entity reference to
+     * the current user as the creator of the instance.
+     */
+    public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
+        parent::preCreate($storage_controller, $values);
+        $values += array(
+            'user_id' => \Drupal::currentUser()->id(),
+        );
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getCreatedTime() {
+        return $this->get('created')->value;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getChangedTime() {
+        return $this->get('changed')->value;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function setChangedTime($timestamp) {
+        $this->set('changed', $timestamp);
+        return $this;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getChangedTimeAcrossTranslations() {
+        $changed = $this->getUntranslated()->getChangedTime();
+        foreach ($this->getTranslationLanguages(FALSE) as $language) {
+            $translation_changed = $this->getTranslation($language->getId())
+                ->getChangedTime();
+            $changed = max($translation_changed, $changed);
+        }
+        return $changed;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getOwner() {
+        return $this->get('user_id')->entity;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function getOwnerId() {
+        return $this->get('user_id')->target_id;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function setOwnerId($uid) {
+        $this->set('user_id', $uid);
+        return $this;
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function setOwner(UserInterface $account) {
+        $this->set('user_id', $account->id());
+        return $this;
+    }
+    /**
+     * {@inheritdoc}
+     *
+     * Define the field properties here.
+     *
+     * Field name, type and size determine the table structure.
+     *
+     * In addition, we can define how the field and its content can be manipulated
+     * in the GUI. The behaviour of the widgets used can be determined here.
+     */
+    public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+        // Standard field, used as unique if primary index.
+        $fields['id'] = BaseFieldDefinition::create('integer')
+            ->setLabel(t('ID'))
+            ->setDescription(t('The ID of the event entity.'))
+            ->setReadOnly(TRUE);
+        // Standard field, unique outside of the scope of the current project.
+        $fields['uuid'] = BaseFieldDefinition::create('uuid')
+            ->setLabel(t('UUID'))
+            ->setDescription(t('The UUID of the event entity.'))
+            ->setReadOnly(TRUE);
+        // Name field for the contact.
+        // We set display options for the view as well as the form.
+        // Users with correct privileges can change the view and edit configuration.
+        $fields['e_name'] = BaseFieldDefinition::create('string')
+            ->setLabel(t('Event name'))
+            ->setDescription(t('The name of the event'))
+            ->setSettings(array(
+                'default_value' => '',
+                'max_length' => 255,
+                'text_processing' => 0,
+            ))
+            ->setRequired(TRUE)
+            ->setDisplayOptions('view', array(
+                'label' => 'above',
+                'type' => 'string',
+                'weight' => 1,
+            ))
+            ->setDisplayOptions('form', array(
+                'type' => 'string',
+                'weight' => 1,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['e_description'] = BaseFieldDefinition::create('string_long')
+            ->setLabel(t('Description'))
+            ->setDescription(t('The description of the event'))
+            ->setSettings(array(
+                'default_value' => '',
+                'text_processing' => 0,
+            ))
+            ->setRequired(TRUE)
+            ->setDisplayOptions('view', array(
+                'label' => 'above',
+                'type' => 'string',
+                'weight' => 2,
+            ))
+            ->setDisplayOptions('form', array(
+                'type' => 'string',
+                'weight' => 2,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['address'] = BaseFieldDefinition::create('address')
+            ->setLabel(t('Venue'))
+            ->setDescription(t('The address of the event'))
+            ->setRevisionable(TRUE)
+            ->setDisplayOptions('view', array(
+                'label' => 'hidden',
+                'type' => 'address_default',
+                'weight' => 3,
+            ))
+            ->setRequired(TRUE)
+            ->setDisplayOptions('form', array(
+                'type' => 'address_default',
+                'weight' => 3,
+            ))
+            ->setSettings(array(
+                'fields' => array(
+                    'countryCode' => 'AU',
+                    'givenName' => FALSE,
+                    'additionalName' => FALSE,
+                    'familyName' => FALSE,
+                    'organization' => FALSE,
+                    'addressLine1' => 'addressLine1',
+                    'addressLine2' => 'addressLine2',
+                    'locality' => 'locality',
+                    'dependentLocality' => 'dependentLocality',
+                    'administrativeArea' => 'administrativeArea',
+                    'postalCode' => 'postalCode',
+                    'sortingCode' => 'sortingCode',
+                ),
+            ));
+        $fields['e_datetime_member'] = BaseFieldDefinition::create('datetime')
+            ->setLabel(t('Members and Friends - <b>Start Date and Time</b>'))
+            ->setDescription(t('The start date and time of the event for members and friends'))
+            ->setDisplayOptions('view', array(
+                'label' => 'above',
+                'type' => 'string',
+                'weight' => 4,
+            ))
+            ->setDisplayOptions('form', array(
+                'type' => 'date',
+                'weight' => 4,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['e_datetime_member_end'] = BaseFieldDefinition::create('datetime')
+            ->setLabel(t('Members and Friends - <b>End Date and Time</b>'))
+            ->setDescription(t('The end date and time of the event for members and friends'))
+            ->setDisplayOptions('view', array(
+                'label' => 'above',
+                'type' => 'string',
+                'weight' => 5,
+            ))
+            ->setDisplayOptions('form', array(
+                'type' => 'date',
+                'weight' => 5,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['e_datetime_general'] = BaseFieldDefinition::create('datetime')
+            ->setLabel(t('General Public - <b>Start Date and Time</b> <font color="red" size="2%">*Required if this is a public event</font>'))
+            ->setDescription(t('The start date and time of the event for general public'))
+            ->setDisplayOptions('view', array(
+                'label' => 'above',
+                'type' => 'string',
+                'weight' => 6,
+            ))
+            ->setRevisionable(TRUE)
+            ->setDisplayOptions('form', array(
+                'type' => 'date',
+                'weight' => 6,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['e_datetime_general_end'] = BaseFieldDefinition::create('datetime')
+            ->setLabel(t('General Public - <b>End Date and Time</b> <font color="red" size="2%">*Required if this is a public event</font>'))
+            ->setDescription(t('The end date and time of the event for general public'))
+            ->setDisplayOptions('view', array(
+                'label' => 'above',
+                'type' => 'string',
+                'weight' => 7,
+            ))
+            ->setRevisionable(TRUE)
+            ->setDisplayOptions('form', array(
+                'type' => 'date',
+                'weight' => 7,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['e_image'] = BaseFieldDefinition::create('image')
+            ->setLabel(t('Image'))
+            ->setDescription(t('Image regarding the event'))
+            ->setSettings([
+                'file_directory' => 'IMAGE_FOLDER',
+                'alt_field_required' => FALSE,
+                'file_extensions' => 'png jpg jpeg',
+            ])
+            ->setDisplayOptions('view', array(
+                'label' => 'hidden',
+                'type' => 'default',
+                'weight' => 2,
+            ))
+            ->setDisplayOptions('form', array(
+                'label' => 'hidden',
+                'type' => 'image_image',
+                'weight' => 2,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        // The form presents a auto complete field for the assets.
+        $fields['asset_id'] = BaseFieldDefinition::create('entity_reference')
+            ->setLabel(t('Asset Name'))
+            ->setDescription(t('The Name of the associated asset.'))
+            ->setSetting('target_type', 'asset')
+            ->setSetting('handler', 'default')
+            ->setCardinality(FieldStorageDefinitionInterface::CARDINALITY_UNLIMITED)
+            ->setDisplayOptions('view', array(
+                'label' => 'above',
+                'type' => 'entity_reference',
+                'weight' => 3,
+            ))
+            ->setDisplayOptions('form', array(
+                'type' => 'entity_reference_autocomplete',
+                'settings' => array(
+                    'match_operator' => 'CONTAINS',
+                    'size' => 60,
+                    'autocomplete_type' => 'tags',
+                    'placeholder' => '',
+                ),
+                'weight' => 3,
+            ))
+            ->setDisplayConfigurable('form', TRUE)
+            ->setDisplayConfigurable('view', TRUE);
+
+        $fields['e_email_body'] = BaseFieldDefinition::create('string_long')
+            ->setLabel(t('Description'))
+            ->setDescription(t('The description of the event'))
+            ->setSettings(array(
+                'default_value' => 'ABC',
+                'text_processing' => 0,
+            ));
+
+        $fields['langcode'] = BaseFieldDefinition::create('language')
+            ->setLabel(t('Language code'))
+            ->setDescription(t('The language code of event entity.'));
+        $fields['created'] = BaseFieldDefinition::create('created')
+            ->setLabel(t('Created'))
+            ->setDescription(t('The time that the entity was created.'));
+        $fields['changed'] = BaseFieldDefinition::create('changed')
+            ->setLabel(t('Changed'))
+            ->setDescription(t('The time that the entity was last edited.'));
+        return $fields;
+    }
+}
+?>
---
+++ src/EventInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Drupal\manage_event;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\user\EntityOwnerInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+/**
+ * Provides an interface defining a Event entity.
+ * @ingroup manage_event
+ */
+interface EventInterface extends ContentEntityInterface, EntityOwnerInterface, EntityChangedInterface {
+}
+?>
