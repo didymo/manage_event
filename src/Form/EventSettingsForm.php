9bb5f591394fbda76c81e0b539a6de6802f96a27
rotaryuow <32447014+rotaryuow@users.noreply.github.com>
2017-10-31 17:13:45 +1100

    Modified code to comply with Drupal coding standards

--- src/Form/EventSettingsForm.php
+++ src/Form/EventSettingsForm.php
@@ -1,46 +1,54 @@
 <?php
+
 namespace Drupal\manage_event\Form;
+
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+
 /**
  * Class EventSettingsForm.
+ *
  * @package Drupal\manage_event\Form
  * @ingroup manage_event
  */
 class EventSettingsForm extends FormBase {
-    /**
-     * Returns a unique string identifying the form.
-     *
-     * @return string
-     *   The unique string identifying the form.
-     */
-    public function getFormId() {
-        return 'event_settings';
-    }
-    /**
-     * Form submission handler.
-     *
-     * @param array $form
-     *   An associative array containing the structure of the form.
-     * @param FormStateInterface $form_state
-     *   An associative array containing the current state of the form.
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
-        // Empty implementation of the abstract submit class.
-    }
-    /**
-     * Define the form used for Event settings.
-     * @return array
-     *   Form definition array.
-     *
-     * @param array $form
-     *   An associative array containing the structure of the form.
-     * @param FormStateInterface $form_state
-     *   An associative array containing the current state of the form.
-     */
-    public function buildForm(array $form, FormStateInterface $form_state) {
-        $form['event_settings']['#markup'] = 'Settings form for Event. Manage field settings here.';
-        return $form;
-    }
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'event_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   An associative array containing the current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+  /**
+   * Define the form used for Event settings.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   An associative array containing the current state of the form.
+   *
+   * @return array
+   *   Form definition array.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['event_settings']['#markup'] = 'Settings form for Event. Manage field settings here.';
+    return $form;
+  }
+
 }
-?>
